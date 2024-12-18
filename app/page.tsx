1201c40dd41674023bcde6ca9aea0ba704bc212d
Corey Munn <michael.munn@secunetics.com>
2024-12-18 10:53:52 -0500

    setup project and create landing page, with some framer motion animations and info

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,177 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+const features = [
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Blazing Fast",
+    description:
+      "Experience lightning-fast answers to your document queries, ensuring you get the information you need instantly.",
+    icon: ZapIcon,
+  },
+  {
+    name: "Chat Memory",
+    description:
+      "The chatbot remembers previous interactions, providing a seamless and personalized experience",
+    icon: BrainCogIcon,
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description:
+      "Engage with your PDFs like never before using our intuitive and interactive viewer.",
+    icon: EyeIcon,
+  },
+  {
+    name: "Cloud Backup",
+    description:
+      "Your documents are safely backed up on the cloud, protected from loss or corruption",
+    icon: ServerCogIcon,
+  },
+  {
+    name: "Responsiveness Across Devices",
+    description:
+      "Access and chat with PDFs seamlessly on any device, from desktop to mobile.",
+    icon: MonitorSmartphoneIcon,
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inView, setInView] = useState(false);
+  const divRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // Check if the div is in view
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setInView(true); // Start animation when in view
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Trigger when 10% of the div is in view
+      }
+    );
+
+    if (divRef.current) {
+      observer.observe(divRef.current);
+    }
+
+    // Clean up the observer
+    return () => {
+      if (divRef.current) {
+        observer.unobserve(divRef.current);
+      }
+    };
+  }, []);
+
+  // Animation variants for the child divs (individual divs)
+  const fadeInVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5, // Duration for each item
+      },
+    },
+  };
+
+  // Animation variant for the parent grid
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.2, // Stagger the children with 0.2s delay
+      },
+    },
+  };
+  return (
+    <main className="flex-1 bg-gradient-to-bl from-white to-indigo-400 overflow-scroll p-2 lg:p-4">
+      <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl mx-auto max-w-6xl px-6 lg:px-8 ">
+        {/* top section of landing page */}
+        <section className="flex flex-col justify-center items-center text-center">
+          <h2 className="text-base font-semibold leading-7 text-primary">
+            Your Interactive Document Companion
+          </h2>
+          <p className="mt-2 font-bold tracking-tight text-gray-900 text-3xl sm:text-6xl">
+            Transform your PDF into Interactive Conversations
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Introducing{" "}
+            <span className="text-primary font-bold">Documentz.chat!</span>
+            <br></br>
+            <br></br>
+            Upload your doucment, our chatbot will answer quesetions, summarize
+            content, and answer all your questions. Ideal for students,
+            professionals, and everyone with a really long PDF to read.
+            <span className="text-primary font-bold">Documentsz.chat</span>{" "}
+            turns static documents into{" "}
+            <span className="font-bold">dynamic conversations</span> enhancing
+            productivity up to 10x.
+          </p>
+          <Button asChild className="shadow-md my-6">
+            <Link href={"/dashboard"}>Get Started</Link>
+          </Button>
+
+          <div className="overflow-hidden pt-16">
+            <div className="px-6 lg:px-8">
+              <Image
+                alt="app screenshot"
+                src={"https://i.imgur.com/VciRSTI.jpeg"}
+                width={2400}
+                height={1400}
+                className="rounded-xl shadow-2xl ring-1 ring-gray-900"
+              />
+              <div className="relative">
+                <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* features section */}
+        <section className="mt-16 px-6 md:mt-24 md:px-8">
+          <motion.dl
+            className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-10 leading-7 text-gray-600"
+            variants={containerVariants}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+            ref={divRef}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            {features.map((feature) => (
+              <motion.div
+                key={feature.name}
+                className="flex flex-row items-center gap-4"
+                variants={fadeInVariants}
+              >
+                <dt className="font-semibold text-gray-900">
+                  <feature.icon className="h-8 w-8" />
+                </dt>
+                <dd>{feature.description}</dd>
+              </motion.div>
+            ))}
+          </motion.dl>
+        </section>
+      </div>
+    </main>
   );
 }
