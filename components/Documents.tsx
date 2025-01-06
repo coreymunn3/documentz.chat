ae1f01b12417c6aaa9e5f21715d4d46d97dc83a8
Corey Munn <michael.munn@secunetics.com>
2025-01-06 10:32:07 -0500

    actually render documents instead of placeholders

--- components/Documents.tsx
+++ components/Documents.tsx
@@ -1,9 +1,21 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { DownloadCloudIcon, PlusCircleIcon, Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import { DocumentData } from "firebase/firestore";
+import { Skeleton } from "./ui/skeleton";
+import byteSize from "byte-size";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 const placeholderDocuments = [
   {
@@ -18,8 +30,28 @@ const placeholderDocuments = [
 
 const Documents = () => {
   const router = useRouter();
-  const user = useUser();
-  console.log(user);
+  const { user } = useUser();
+  const [documents, setDocuments] = useState<DocumentData[]>([]);
+  console.log(documents);
+
+  const [snapshot, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, "users", user.id, "files"),
+        orderBy("createdAt", "desc")
+      )
+  );
+
+  /**
+   * format user docs from a snapshot to the actual data
+   */
+  useEffect(() => {
+    if (snapshot) {
+      setDocuments(
+        snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+      );
+    }
+  }, [snapshot]);
 
   const handleAddDocument = () => {
     // eventually, check if user is free tier, if they are over file limit, push to upgrade screen
@@ -28,16 +60,46 @@ const Documents = () => {
 
   return (
     <div className="flex  flex-wrap">
+      {/* loading skeletons */}
+      {loading && (
+        <div className="flex space-x-2">
+          <Skeleton className="h-80 w-64 rounded-xl" />
+          <Skeleton className="h-80 w-64 rounded-xl" />
+          <Skeleton className="h-80 w-64 rounded-xl" />
+        </div>
+      )}
+      {/* render the add document button first */}
+      <Button
+        className="flex flex-col items-center justify-center w-64 h-80 rounded-xl drop-shadow-md mr-2 mb-2 text-slate-400 bg-slate-200 hover:text-white dark:bg-slate-700 hover:dark:bg-slate-600"
+        onClick={handleAddDocument}
+      >
+        <PlusCircleIcon className="h-16 w-16" />
+        Add A Document
+      </Button>
       {/* render out the documents */}
-      {placeholderDocuments.map((doc) => (
+      {documents.map((doc) => (
         <Button
-          key={doc.filename}
+          key={doc.data.name}
           variant="ghost"
+          // navigate to the file page
+          onClick={() => router.push(`dashboard/files/${doc.id}`)}
           className="w-64 h-80 rounded-xl drop-shadow-md flex flex-col items-start justify-between mr-2 mb-2  bg-white hover:bg-slate-100 dark:bg-slate-800 hover:dark:bg-slate-700"
         >
           <div className="text-left">
-            <p className="">{doc.filename}</p>
-            <p className="font-extralight text-slate-400">{doc.filesize}</p>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <p className="w-56 overflow-hidden text-ellipsis">
+                    {doc.data.name}
+                  </p>
+                </TooltipTrigger>
+                <TooltipContent>{doc.data.name}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <p className="font-extralight text-slate-400">
+              {byteSize(doc.data.size).value} kB
+            </p>
           </div>
           <div className="flex flex-row space-x-2 w-full justify-end">
             {/* download */}
@@ -51,14 +113,6 @@ const Documents = () => {
           </div>
         </Button>
       ))}
-      {/* render the add document button */}
-      <Button
-        className="flex flex-col items-center justify-center w-64 h-80 rounded-xl drop-shadow-md text-slate-400 bg-slate-200 hover:text-white dark:bg-slate-700 hover:dark:bg-slate-600"
-        onClick={handleAddDocument}
-      >
-        <PlusCircleIcon className="h-16 w-16" />
-        Add A Document
-      </Button>
     </div>
   );
 };
