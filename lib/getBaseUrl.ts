6465247bef8340d0d748a4346319826c653d5b1b
Corey Munn <michael.munn@secunetics.com>
2025-03-16 23:07:06 -0400

    ensure https

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -5,7 +5,7 @@ if (!process.env.VERCEL_URL) {
 const getBaseUrl = () => {
   return process.env.NODE_ENV === "development"
     ? "http://localhost:3030"
-    : process.env.VERCEL_URL;
+    : `https://${process.env.VERCEL_URL}`;
 };
 
 export default getBaseUrl;
